3bb02b1f67f16af578f7eacd02ce7a8069850c43
Wesley Moore <wez@wezm.net>
2010-11-10 08:42:02 +1100

    Add vless alias for vim as a pager

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -1,4 +1,4 @@
-#RSpec
+# RSpec
 alias spec="spec -c"
 
 # Git
@@ -6,6 +6,9 @@ alias gs='git status'
 alias ga='git add'
 alias gco='git checkout'
 alias gd="git diff"
+
+# Vim pager
+alias vless="/usr/share/vim/vim72/macros/less.sh"
 
 # Rails 3
 alias rc="rails console"
