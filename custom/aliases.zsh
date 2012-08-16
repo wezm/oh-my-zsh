5d5fd22dc45f7205f95e0761e48ccf23093086a1
Wesley Moore <wes@wezm.net>
2012-08-16 13:42:48 +1000

    Add Capistrano aliases, update vless

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -9,4 +9,12 @@ alias gf='git fetch'
 alias gco='git checkout'
 
 # Vim pager
-alias vless="/usr/share/vim/vim72/macros/less.sh"
+alias vless="/usr/share/vim/vim73/macros/less.sh"
+
+# Capistrano
+alias capss="cap staging ssh"
+alias capsc="cap staging console"
+alias capsd="cap staging deploy"
+alias capps="cap production ssh"
+alias cappc="cap production console"
+alias cappd="cap production deploy"
